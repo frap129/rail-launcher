717b282e2989ad014718546ef6aacfedb060ed62
Joe Maples <joe@maples.dev>
2024-10-27 18:39:20 -0400

    feat(launcher): THE RAIL

--- feature/launcher/src/main/java/feature/launcher/LauncherScreen.kt
+++ feature/launcher/src/main/java/feature/launcher/LauncherScreen.kt
@@ -2,19 +2,30 @@ package feature.launcher
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -22,6 +33,9 @@ import androidx.navigation.NavController
 import com.google.accompanist.drawablepainter.DrawablePainter
 import core.apprepo.App
 import core.ui.model.data.Destination
+import kotlin.math.max
+import kotlin.math.min
+import kotlinx.coroutines.launch
 import org.koin.androidx.compose.koinViewModel
 
 val launcherDestination = Destination(
@@ -34,21 +48,34 @@ val launcherDestination = Destination(
 @Composable
 fun LauncherScreen(navController: NavController, viewModel: LauncherViewModel = koinViewModel()) {
     val launcherItems = viewModel.launcherItems.collectAsState(emptyMap<Char, List<App>>())
+    val launcherScrollState = rememberLazyListState()
 
-    LazyColumn(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.Start,
-        verticalArrangement = Arrangement.Top
-    ) {
-        items(launcherItems.value.keys.toList()) { key ->
-            ItemGroup("$key", launcherItems.value[key]!!)
+    Box {
+        LazyColumn(
+            modifier = Modifier.fillMaxSize().align(Alignment.Center),
+            state = launcherScrollState,
+            horizontalAlignment = Alignment.Start,
+            verticalArrangement = Arrangement.Top
+        ) {
+            items(launcherItems.value.keys.toList()) { key ->
+                ItemGroup(
+                    label = "$key",
+                    items = launcherItems.value[key]!!
+                )
+            }
         }
+        AlphaNumericScrollbar(
+            modifier = Modifier.align(Alignment.CenterEnd),
+            listState = launcherScrollState,
+            keys = launcherItems.value.keys.toList()
+        )
     }
 }
 
 @Composable
 fun ItemGroup(label: String, items: List<App>) {
     val context = LocalContext.current
+
     Text(
         text = label,
         textAlign = TextAlign.Center,
@@ -73,6 +100,45 @@ fun ItemGroup(label: String, items: List<App>) {
                 )
                 Text(item.label)
             }
+        }
+    }
+}
+
+@Composable
+fun AlphaNumericScrollbar(modifier: Modifier = Modifier, listState: LazyListState, keys: List<Char>) {
+    var selectedItemIndex by remember { mutableIntStateOf(-1) }
+    var verticalOffset by remember { mutableFloatStateOf(0f) }
+    val scope = rememberCoroutineScope()
+
+    Column(
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .pointerInput(Unit) {
+                detectDragGestures(
+                    onDragStart = {
+                    }
+                ) { change, dragAmount ->
+                    verticalOffset += dragAmount.y
+                    val itemIndex = max(min((verticalOffset / 48.dp.toPx()).toInt(), keys.size - 1), 0)
+                    if (itemIndex != selectedItemIndex) {
+                        scope.launch {
+                            listState.scrollToItem(
+                                index = itemIndex,
+                                scrollOffset = -500
+                            )
+                        }
+                    }
+                    selectedItemIndex = itemIndex
+                }
+            }
+    ) {
+        keys.forEach { label ->
+            Text(
+                text = "$label",
+                textAlign = TextAlign.Center,
+                modifier = Modifier.size(48.dp)
+            )
         }
     }
 }
