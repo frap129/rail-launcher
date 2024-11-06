d81a93f7180e4e8e631fedf7fad0552a8f37cfc5
Joe Maples <joe@maples.dev>
2024-11-06 18:17:41 -0500

    feat(launcher/data): Add item bottom sheet with App Info

--- core/data/src/main/java/core/data/apps/App.kt
+++ core/data/src/main/java/core/data/apps/App.kt
@@ -1,8 +1,11 @@
 package core.data.apps
 
 import android.content.Context
+import android.content.Intent
 import android.content.pm.LauncherActivityInfo
+import android.net.Uri
 import android.os.UserHandle
+import android.provider.Settings
 import core.data.rail.RailItem
 
 class App(info: LauncherActivityInfo, val profile: UserHandle) : RailItem() {
@@ -15,4 +18,10 @@ class App(info: LauncherActivityInfo, val profile: UserHandle) : RailItem() {
     override fun launch(context: Context) = context.startActivity(
         context.packageManager.getLaunchIntentForPackage(packageName)
     )
+
+    fun openAppInfo(context: Context) {
+        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+        intent.data = Uri.parse("package:$packageName")
+        context.startActivity(intent)
+    }
 }
--- feature/launcher/src/main/java/feature/launcher/LauncherScreen.kt
+++ feature/launcher/src/main/java/feature/launcher/LauncherScreen.kt
@@ -2,12 +2,14 @@ package feature.launcher
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
@@ -16,18 +18,24 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.IntOffset
@@ -35,6 +43,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.google.accompanist.drawablepainter.DrawablePainter
+import core.data.apps.App
 import core.data.rail.RailItem
 import core.ui.composables.scrollrail.ScrollRail
 import core.ui.model.data.Destination
@@ -106,7 +115,6 @@ fun LauncherScreen(navController: NavController, viewModel: LauncherViewModel = 
 
 @Composable
 fun itemGroup(label: String, items: List<RailItem>): MutableState<Boolean> {
-    val context = LocalContext.current
     val visible = remember { mutableStateOf(true) }
 
     Text(
@@ -124,24 +132,91 @@ fun itemGroup(label: String, items: List<RailItem>): MutableState<Boolean> {
         modifier = Modifier.alpha(if (visible.value) 1f else 0f)
     ) {
         items.forEach { item ->
+            LauncherItem(item)
+        }
+    }
+    return visible
+}
+
+@Composable
+fun LauncherItem(item: RailItem) {
+    val context = LocalContext.current
+    var showBottomSheet by remember { mutableStateOf(false) }
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Start,
+        modifier = Modifier
+            .fillMaxWidth()
+            .pointerInput(Unit) {
+                detectTapGestures(onTap = { item.launch(context) }, onLongPress = { showBottomSheet = true })
+                item.launch(context)
+            }
+    ) {
+        Image(
+            modifier = Modifier.size(64.dp).padding(10.dp),
+            painter = DrawablePainter(item.getIcon(context)),
+            contentDescription = item.name
+        )
+        Text(
+            text = item.name,
+            fontSize = 20.sp
+        )
+        if (showBottomSheet) {
+            LauncherItemBottomSheet(
+                item = item,
+                onDismissRequest = { showBottomSheet = false }
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LauncherItemBottomSheet(item: RailItem, onDismissRequest: () -> Unit) {
+    val context = LocalContext.current
+
+    ModalBottomSheet(
+        onDismissRequest = onDismissRequest
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(20.dp, 10.dp)
+        ) {
+            Image(
+                modifier = Modifier.size(64.dp).padding(10.dp),
+                painter = DrawablePainter(item.getIcon(context)),
+                contentDescription = item.name
+            )
+            Text(
+                text = item.name,
+                fontSize = 28.sp
+            )
+        }
+
+        if (item is App) {
             Row(
                 verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.Start,
-                modifier = Modifier.clickable {
-                    item.launch(context)
-                }
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(20.dp, 10.dp).clickable {
+                        item.openAppInfo(context)
+                    }
             ) {
                 Image(
-                    modifier = Modifier.size(64.dp).padding(10.dp),
-                    painter = DrawablePainter(item.getIcon(context)),
-                    contentDescription = item.name
+                    modifier = Modifier.size(48.dp).padding(10.dp),
+                    painter = painterResource(android.R.drawable.ic_dialog_info),
+                    contentDescription = "App Info"
                 )
                 Text(
-                    text = item.name,
+                    text = "App Info",
                     fontSize = 20.sp
                 )
             }
         }
     }
-    return visible
 }
