c2a5ccdbfe49b8d7754cf5255f514383ddd5ef9a
Joe Maples <joe@maples.dev>
2024-11-07 15:21:54 -0500

    refactor(core:ui): Use pointerInput for ScrollRail

--- core/ui/src/main/java/core/ui/composables/scrollrail/ScrollRail.kt
+++ core/ui/src/main/java/core/ui/composables/scrollrail/ScrollRail.kt
@@ -1,6 +1,5 @@
 package core.ui.composables.scrollrail
 
-import android.view.MotionEvent
 import androidx.compose.animation.core.animateIntOffsetAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -20,11 +19,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
-import androidx.compose.ui.input.pointer.RequestDisallowInterceptTouchEvent
-import androidx.compose.ui.input.pointer.pointerInteropFilter
+import androidx.compose.ui.input.pointer.PointerEventType
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.text.font.FontWeight
@@ -50,12 +48,10 @@ import kotlinx.coroutines.launch
  * @param modifier the modifier applied to the column
  * @param scrollRailHelper an object of  [core.ui.composables.scrollrail.ScrollRailHelper]
  */
-@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun ScrollRail(modifier: Modifier = Modifier, scrollRailHelper: ScrollRailHelper) {
     val context = LocalContext.current
     val scope = rememberCoroutineScope()
-    val disallowIntercept = remember { RequestDisallowInterceptTouchEvent() }
     val hapticFeedback = LocalHapticFeedback.current
     val density = context.resources.displayMetrics.density
     val itemHeight = 20.dp
@@ -71,44 +67,46 @@ fun ScrollRail(modifier: Modifier = Modifier, scrollRailHelper: ScrollRailHelper
     Row(
         horizontalArrangement = Arrangement.End,
         modifier = modifier
-            .pointerInteropFilter(disallowIntercept) { event ->
-                verticalOffset = event.y
-                horizontalOffset = event.x
-                if (verticalOffset < railOffset) {
-                    railOffset = verticalOffset
-                } else if (verticalOffset > railOffset + railHeight) {
-                    railOffset = verticalOffset - railHeight
-                }
+            .pointerInput(Unit) {
+                awaitPointerEventScope {
+                    while (true) {
+                        val event = awaitPointerEvent()
+                        verticalOffset = event.changes.first().position.y
+                        horizontalOffset = event.changes.first().position.x
+                        if (verticalOffset < railOffset) {
+                            railOffset = verticalOffset
+                        } else if (verticalOffset > railOffset + railHeight) {
+                            railOffset = verticalOffset - railHeight
+                        }
 
-                val calculatedIndex = ((verticalOffset - railOffset) / (itemHeight.value * density)).toInt()
-                val itemIndex = max(min(calculatedIndex, scrollRailHelper.railItems.size - 1), 0)
+                        val calculatedIndex = ((verticalOffset - railOffset) / (itemHeight.value * density)).toInt()
+                        val itemIndex = max(min(calculatedIndex, scrollRailHelper.railItems.size - 1), 0)
 
-                when (event.action) {
-                    MotionEvent.ACTION_DOWN -> {
-                        hapticFeedback.performHapticFeedback(HapticFeedbackType.TextHandleMove)
-                        scope.launch { scrollRailHelper.onScrollStarted(itemIndex) }
-                        selectedItemIndex = itemIndex
-                    }
+                        when (event.type) {
+                            PointerEventType.Press -> {
+                                hapticFeedback.performHapticFeedback(HapticFeedbackType.TextHandleMove)
+                                scope.launch { scrollRailHelper.onScrollStarted(itemIndex) }
+                                selectedItemIndex = itemIndex
+                            }
 
-                    MotionEvent.ACTION_MOVE -> {
-                        if (itemIndex != selectedItemIndex) {
-                            hapticFeedback.performHapticFeedback(HapticFeedbackType.TextHandleMove)
-                            scope.launch { scrollRailHelper.onScroll(itemIndex, selectedItemIndex) }
-                            selectedItemIndex = itemIndex
+                            PointerEventType.Move -> {
+                                if (itemIndex != selectedItemIndex) {
+                                    hapticFeedback.performHapticFeedback(HapticFeedbackType.TextHandleMove)
+                                    scope.launch { scrollRailHelper.onScroll(itemIndex, selectedItemIndex) }
+                                    selectedItemIndex = itemIndex
+                                }
+                            }
+
+                            PointerEventType.Unknown, PointerEventType.Release -> {
+                                scope.launch { scrollRailHelper.onScrollEnded(selectedItemIndex) }
+                                selectedItemIndex = -1
+                                verticalOffset = 0f
+                                horizontalOffset = 0f
+                                railOffset = 0f
+                            }
                         }
                     }
-
-                    else -> {
-                        scope.launch { scrollRailHelper.onScrollEnded(selectedItemIndex) }
-                        selectedItemIndex = -1
-                        verticalOffset = 0f
-                        horizontalOffset = 0f
-                        railOffset = 0f
-                    }
                 }
-
-                disallowIntercept.invoke(true)
-                true
             }
     ) {
         if (selectedItemIndex >= 0) {
