03be77210d496350fe1326f77ffc41c8ab08949a
Joe Maples <joe@maples.dev>
2024-11-20 16:48:57 -0500

    style(launcher): Clean up text styles

--- feature/launcher/build.gradle.kts
+++ feature/launcher/build.gradle.kts
@@ -10,4 +10,5 @@ dependencies {
     implementation(project(":core:ui"))
     implementation(project(":core:data"))
     implementation(project(":core:util"))
+    implementation(libs.icons.fontawesome)
 }
--- feature/launcher/src/main/java/feature/launcher/LauncherScreen.kt
+++ feature/launcher/src/main/java/feature/launcher/LauncherScreen.kt
@@ -26,12 +26,15 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -40,12 +43,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.style.TextAlign
@@ -55,6 +58,9 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import coil3.compose.AsyncImage
+import compose.icons.FontAwesomeIcons
+import compose.icons.fontawesomeicons.Solid
+import compose.icons.fontawesomeicons.solid.InfoCircle
 import core.data.apps.model.App
 import core.data.launchables.model.Launchable
 import core.data.launchables.model.LaunchableGroup
@@ -196,15 +202,16 @@ fun LauncherList(
 @Composable
 fun LauncherItemGroup(modifier: Modifier = Modifier, group: LaunchableGroup) {
     Column(modifier = modifier) {
-        OutlinedText(
-            text = group.name,
-            fontSize = 26.sp,
-            textAlign = TextAlign.Start,
-            fontWeight = FontWeight.SemiBold,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(20.dp, 24.dp, 0.dp, 8.dp)
-        )
+        CompositionLocalProvider(LocalTextStyle provides MaterialTheme.typography.headlineMedium) {
+            OutlinedText(
+                text = group.name,
+                textAlign = TextAlign.Start,
+                fontWeight = FontWeight.SemiBold,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(20.dp, 24.dp, 0.dp, 8.dp)
+            )
+        }
         group.items.forEach { item ->
             LauncherItem(item)
         }
@@ -216,30 +223,31 @@ fun LauncherItem(item: Launchable, viewModel: LauncherViewModel = koinViewModel(
     val context = LocalContext.current
     val hapticFeedback = LocalHapticFeedback.current
 
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.Start,
-        modifier = Modifier
-            .fillMaxWidth()
-            .pointerInput(Unit) {
-                detectTapGestures(
-                    onTap = { item.launch(context) },
-                    onLongPress = {
-                        viewModel.openItemMenu(item)
-                        hapticFeedback.performHapticFeedback(HapticFeedbackType.LongPress)
-                    }
-                )
-            }
-    ) {
-        AsyncImage(
-            modifier = Modifier.size(64.dp).padding(10.dp),
-            model = item.icon,
-            contentDescription = item.name
-        )
-        OutlinedText(
-            text = item.name,
-            fontSize = 18.sp
-        )
+    CompositionLocalProvider(LocalTextStyle provides MaterialTheme.typography.titleMedium) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start,
+            modifier = Modifier
+                .fillMaxWidth()
+                .pointerInput(Unit) {
+                    detectTapGestures(
+                        onTap = { item.launch(context) },
+                        onLongPress = {
+                            viewModel.openItemMenu(item)
+                            hapticFeedback.performHapticFeedback(HapticFeedbackType.LongPress)
+                        }
+                    )
+                }
+        ) {
+            AsyncImage(
+                modifier = Modifier.size(64.dp).padding(10.dp),
+                model = item.icon,
+                contentDescription = item.name
+            )
+            OutlinedText(
+                text = item.name
+            )
+        }
     }
 }
 
@@ -247,49 +255,50 @@ fun LauncherItem(item: Launchable, viewModel: LauncherViewModel = koinViewModel(
 fun LauncherItemMenu(item: Launchable, viewModel: LauncherViewModel = koinViewModel()) {
     val context = LocalContext.current
 
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.Start,
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(20.dp, 10.dp)
-    ) {
-        AsyncImage(
-            modifier = Modifier
-                .size(64.dp)
-                .padding(10.dp),
-            model = item.icon,
-            contentDescription = item.name
-        )
-
-        Text(
-            text = item.name,
-            fontSize = 28.sp,
-            modifier = Modifier.clickable {
-                viewModel.openItemRename(item)
-            }
-        )
-    }
-
-    if (item is App) {
+    CompositionLocalProvider(LocalTextStyle provides MaterialTheme.typography.headlineMedium) {
         Row(
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.Start,
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(20.dp, 10.dp).clickable {
-                    item.openAppInfo(context)
-                }
+                .padding(20.dp, 10.dp)
         ) {
-            Image(
-                modifier = Modifier.size(48.dp).padding(10.dp),
-                painter = painterResource(android.R.drawable.ic_dialog_info),
-                contentDescription = "App Info"
+            AsyncImage(
+                modifier = Modifier
+                    .size(64.dp)
+                    .padding(10.dp),
+                model = item.icon,
+                contentDescription = item.name
             )
+
             Text(
-                text = "App Info",
-                fontSize = 20.sp
+                text = item.name,
+                fontSize = 28.sp,
+                modifier = Modifier.clickable {
+                    viewModel.openItemRename(item)
+                }
             )
+        }
+    }
+
+    CompositionLocalProvider(LocalTextStyle provides MaterialTheme.typography.titleLarge) {
+        if (item is App) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Start,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable { item.openAppInfo(context) }
+                    .padding(20.dp, 10.dp)
+            ) {
+                Image(
+                    modifier = Modifier.size(56.dp).padding(10.dp),
+                    imageVector = FontAwesomeIcons.Solid.InfoCircle,
+                    colorFilter = ColorFilter.tint(LocalContentColor.current),
+                    contentDescription = "App Info"
+                )
+                Text("App Info")
+            }
         }
     }
 }
@@ -302,60 +311,52 @@ fun LauncherItemEditName(item: Launchable, viewModel: LauncherViewModel = koinVi
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier.fillMaxWidth()
     ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Start,
-            modifier = Modifier
-                .padding(20.dp, 10.dp)
-        ) {
-            AsyncImage(
+        CompositionLocalProvider(LocalTextStyle provides MaterialTheme.typography.headlineMedium) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Start,
                 modifier = Modifier
-                    .size(64.dp)
-                    .padding(10.dp),
-                model = item.icon,
-                contentDescription = item.name
-            )
-            Text(
-                text = "Rename ${item.name}",
-                fontSize = 24.sp
-            )
+                    .padding(20.dp, 10.dp)
+            ) {
+                AsyncImage(
+                    modifier = Modifier
+                        .size(64.dp)
+                        .padding(10.dp),
+                    model = item.icon,
+                    contentDescription = item.name
+                )
+                Text("Rename ${item.name}")
+            }
         }
 
-        OutlinedTextField(
-            modifier = Modifier
-                .padding(48.dp, 10.dp)
-                .fillMaxWidth(),
-            singleLine = true,
-            shape = CircleShape,
-            textStyle = LocalTextStyle.current.copy(fontSize = 18.sp),
-            value = nameEdit,
-            onValueChange = { nameEdit = it },
-            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done)
-        )
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(10.dp),
-            horizontalArrangement = Arrangement.SpaceEvenly
-        ) {
-            TextButton(
-                onClick = {
-                    nameEdit = item.defaultName
+        CompositionLocalProvider(LocalTextStyle provides MaterialTheme.typography.titleMedium) {
+            OutlinedTextField(
+                modifier = Modifier
+                    .padding(48.dp, 10.dp)
+                    .fillMaxWidth(),
+                singleLine = true,
+                shape = CircleShape,
+                value = nameEdit,
+                onValueChange = { nameEdit = it },
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done)
+            )
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                TextButton(
+                    onClick = { nameEdit = item.defaultName }
+                ) {
+                    Text("Reset")
                 }
-            ) {
-                Text(
-                    text = "Reset",
-                    fontSize = 18.sp
-                )
-            }
 
-            TextButton(
-                onClick = { viewModel.setItemName(item, nameEdit) }
-            ) {
-                Text(
-                    text = "Done",
-                    fontSize = 18.sp
-                )
+                TextButton(
+                    onClick = { viewModel.setItemName(item, nameEdit) }
+                ) {
+                    Text("Done")
+                }
             }
         }
     }
